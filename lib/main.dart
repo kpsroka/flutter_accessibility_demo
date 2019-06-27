c42e39cc0a912f926940c0251a724741e858d792
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2019-06-27 23:31:57 +0200

    Fffffun

--- lib/main.dart
+++ lib/main.dart
@@ -29,16 +29,16 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  TextEditingController _textEditingController =
+      TextEditingController(text: 'Hey hey');
 
   void _incrementCounter() {
-    debugPrint('Increasing to ${_counter + 1}');
     setState(() {
       _counter++;
     });
   }
 
   void _decrementCounter() {
-    debugPrint('Decreasing to ${_counter - 1}');
     setState(() {
       _counter--;
     });
@@ -52,24 +52,82 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             Text(
               'You have pushed the button this many times:',
             ),
             Semantics(
-                label: 'Counter button',
-                hint: 'Press to increase',
-                value: '$_counter',
-                enabled: true,
-                container: true,
-                excludeSemantics: true,
-                onTap: _incrementCounter,
-                onLongPress: _decrementCounter,
+              label: 'Counter button',
+              hint: 'Press to increase',
+              value: '$_counter',
+              onTap: _incrementCounter,
+              //enabled: true,
+              //container: true,
+              //excludeSemantics: true,
+              //onLongPress: _decrementCounter,
+              child: ExcludeSemantics(
                 child: Text(
                   '$_counter',
                   style: Theme.of(context).textTheme.display1,
-                )),
+                ),
+              ),
+            ),
+            MergeSemantics(
+              child: Semantics(
+                onCopy: () {
+                  debugPrint('Hey, where are you taking that?');
+                },
+                onCut: () {
+                  debugPrint('Give it back!');
+                },
+                onPaste: () {
+                  debugPrint('All right, do not do that again though.');
+                },
+                child: TextField(
+                  controller: _textEditingController,
+                  autocorrect: false,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 100,
+              height: 100,
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    width: 50,
+                    height: 50,
+                    child: Semantics(
+                      label: "Red",
+                      child: Container(color: Colors.red),
+                    ),
+                  ),
+                  Positioned(
+                    top: 25,
+                    left: 25,
+                    width: 50,
+                    height: 50,
+                    child: Semantics(
+                      label: "Green",
+                      child: Container(color: Colors.green),
+                    ),
+                  ),
+                  Positioned(
+                    top: 50,
+                    left: 50,
+                    width: 50,
+                    height: 50,
+                    child: Semantics(
+                      label: "Blue",
+                      child: Container(color: Colors.blue),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
